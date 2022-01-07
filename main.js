f9d546e414949e5a2567d46117d1c5bba4c8c47d
Alexander Duffaut <aduffaut@ufl.edu>
2022-01-07 17:59:07 -0500

    importing 3d html and js

--- main.js
+++ main.js
@@ -1,6 +1,110 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(5, 1, 8, 100);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+torus.position.setX(-10);
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(20, 20, 20);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+
+scene.add(pointLight, ambientLight);
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+
+// scene.add(lightHelper, gridHelper);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+const davidTexture = new THREE.TextureLoader().load('david.jpg');
+const david = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 5, 5),
+  new THREE.MeshBasicMaterial({map:davidTexture})
+);
+
+scene.add(david);
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+);
+
+scene.add(moon);
+moon.position.z = 30;
+moon.position.setX(-10);
+
+david.position.z = -5;
+david.position.x = 2;
+
+function moveCamera() {
+ const t = document.body.getBoundingClientRect().top;
+ moon.rotation.x += 0.005;
+ moon.rotation.y += 0.0075;
+ moon.rotation.z += 0.005;
+
+ david.rotation.y += 0.01;
+ david.rotation.z += 0.01;
+
+ camera.position.z = t * -0.01;
+ camera.position.x = t * -0.0002;
+ camera.rotation.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
